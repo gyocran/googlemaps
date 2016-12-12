27099662a2e5d3c7e7e871fb57bb44bb71431117
gyocran <justblaze.0@gmail.com>
2016-12-12 14:43:52 +0000

    mobile final

--- setting.php
+++ setting.php
@@ -1,5 +1,5 @@
 <?php
-define("DB_HOST","localhost");
+define("DB_HOST","52.89.116.249");
 define("DB_USERNAME","george_ocran");
 define("DB_PASSWORD","b3d21261022f6947");
 define("DB_NAME","mobileweb__george_ocran");
